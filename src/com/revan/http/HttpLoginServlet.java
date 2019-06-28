52004d07a7fe44e84367666ebddbbb59cd6f03be
RevanWang <zjqx1991@163.com>
2019-06-28 18:48:11 +0800

    使用注解

---
+++ src/com/revan/http/HttpLoginServlet.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.revan.http;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Desc 	
+ * @author Revan Wang
+ *
+ * @Date Jun 28, 20193:38:54 PM
+ */
+@WebServlet("/http/login")
+public class HttpLoginServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(req);
+	}
+
+}
